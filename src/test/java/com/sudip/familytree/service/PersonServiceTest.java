db1cf9c8cf069c86c37683b9c2c8a6f73364284b
Sudip Bhandari <sudipbhandari@sudipbhandari.local>
2020-05-14 01:49:10 +0530

    sibling test added

--- src/test/java/com/sudip/familytree/service/PersonServiceTest.java
+++ src/test/java/com/sudip/familytree/service/PersonServiceTest.java
@@ -1,7 +1,11 @@
 package com.sudip.familytree.service;
 
+import com.sudip.familytree.entities.Person;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PersonServiceTest {
     PersonService personService;
@@ -11,11 +15,19 @@ public class PersonServiceTest {
         personService = new PersonService();
         personService.addChild("laxmi","sita","Female");
         personService.addChild("sita","barsha","Female");
+        personService.addChild("sita","bibek","Male");
     }
 
     @Test
     public void grandMotherTest(){
         personService.get("barsha").getMother().getMother().equals(personService.get("laxmi"));
+    }
+
+    @Test
+    public void siblingTest(){
+         assert  personService.get("barsha").getMother()
+                 .getChildren()
+                 .containsAll(Arrays.asList(personService.get("barsha"),personService.get("bibek")));
     }
 
 }
