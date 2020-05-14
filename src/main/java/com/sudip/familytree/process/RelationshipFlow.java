efb99d39abac302758bbf3942b3ad4df8d4ba023
Sudip Bhandari <sudipbhandari@sudipbhandari.local>
2020-05-14 21:16:53 +0530

    relationships added

--- src/main/java/com/sudip/familytree/process/RelationshipFlow.java
+++ src/main/java/com/sudip/familytree/process/RelationshipFlow.java
@@ -30,6 +30,31 @@ public class RelationshipFlow {
         RelationShip daughterRelationShip = person -> person.getChildren().stream()
                 .filter(each->each.getGender().equals(Gender.FEMALE))
                 .findAny().get();
+        RelationShip paternalUncle = person -> person.getFather().getFather()
+                .getChildren()
+                .stream()
+                .filter(each->each.getGender().equals(Gender.MALE))
+                .filter(each->!each.equals(person))
+                .findAny().get();
+        RelationShip paternalAunt = person -> person.getFather().getFather()
+                .getChildren().stream()
+                .filter(each->each.getGender().equals(Gender.FEMALE))
+                .findAny().get();
+        RelationShip maternalUncle = person -> person.getMother().getMother()
+                .getChildren().stream()
+                .filter(each->each.getGender().equals(Gender.MALE))
+                .findAny().get();
+        RelationShip sisterInLaw = person -> person.getWife()
+                .getMother()
+                .getChildren().stream()
+                .filter(each->each.getGender().equals(Gender.FEMALE))
+                .findAny().get();
+
+        RelationShip brotherInLaw = person -> person.getHusband()
+                .getMother()
+                .getChildren().stream()
+                .filter(each->each.getGender().equals(Gender.MALE))
+                .findAny().get();
 
 
 
@@ -41,6 +66,12 @@ public class RelationshipFlow {
         relationShipMap.put("Maternal-Aunt",maternalAuntRelationship);
         relationShipMap.put("son",sonRelationship);
         relationShipMap.put("daughter",daughterRelationShip);
+        relationShipMap.put("Paternal-Uncle",paternalUncle);
+        relationShipMap.put("Paternal-Aunt",paternalAunt);
+        relationShipMap.put("Maternal-Uncle",maternalUncle);
+        relationShipMap.put("Sister-In-Law",sisterInLaw);
+        relationShipMap.put("Brother-In-Law",brotherInLaw);
+
 
 
 
