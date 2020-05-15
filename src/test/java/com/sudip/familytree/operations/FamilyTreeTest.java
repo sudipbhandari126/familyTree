5842e07bbcf19c2bb8e3636fdf332de81d5e7f80
Sudip Bhandari <sudipbhandari@sudipbhandari.local>
2020-05-16 02:17:41 +0530

    command factory tests family tree tests

--- src/test/java/com/sudip/familytree/operations/FamilyTreeTest.java
+++ src/test/java/com/sudip/familytree/operations/FamilyTreeTest.java
@@ -1,19 +1,44 @@
 package com.sudip.familytree.operations;
 
+import com.sudip.familytree.entities.Person;
+import com.sudip.familytree.enums.Gender;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FamilyTreeTest {
+    private List<Person> persons;
+
 
     @Before
     public void init() {
-
+        persons = new ArrayList<>();
+        Person ram = new Person("Ram", Gender.MALE);
+        Person sita = new Person("Sita", Gender.FEMALE);
+        persons = Arrays.asList(ram,sita);
     }
 
     @Test
     //todo 1. showing valid names, 2. showing NONE for non existing (single name, multiple names)
-    public void test() {
+    public void familyTreeShowsNameOfPersonIsSuccess() {
+        String names = FamilyTree.getNames(persons);
+        assert names.contains("Ram") && names.contains("Sita");
+    }
 
+    @Test
+    public void familyTreeShowsNoneIfPersonsListIsEmpty(){
+        String names = FamilyTree.getNames(Arrays.asList());
+        assert names.equals("NONE");
+    }
+
+
+    @Test
+    public void familyTreeShowsPersonNameForSinglePerson(){
+        String names = FamilyTree.getNames(Arrays.asList(persons.get(0)));
+        assert names.equals("Ram");
     }
 
 }
